0e235e94d522d011990ff2806572fd7df8ac1280
qus <qus1225@gmail.com>
2017-05-20 22:36:46 +0900

    평균 트랜잭션값을 구하는 기능이 추가되고, json_data를 받아오는 메서드가 따로 분리됩니다

--- app/jobs/block_chain_job.rb
+++ app/jobs/block_chain_job.rb
@@ -1,10 +1,26 @@
 class BlockChainJob < ApplicationJob
   def total_tx_count(block_hash)
-    uri = "https://blockchain.info/block-index/#{block_hash}?format=json"
-    resp = Net::HTTP.get_response(URI.parse(uri))
-    data= JSON.parse(resp.body)
-
+    data = get_json_data(block_hash)
     total_tx = data["tx"]
     return total_tx.size
+  end
+
+  def avg_tx_value(block_hash)
+    data = get_json_data(block_hash)
+    total_tx = data["tx"]
+    total_tx_value = 0
+    total_tx.each do |tx|
+      # out의 value를 합산하면 해당 트랜잭션의 value가 나옴
+      tx["out"].each do |out|
+        total_tx_value += out["value"]
+      end
+    end
+    return total_tx_value / total_tx_count(block_hash).to_f
+  end
+
+  def get_json_data(block_hash)
+    uri = "https://blockchain.info/block-index/#{block_hash}?format=json"
+    resp = Net::HTTP.get_response(URI.parse(uri))
+    JSON.parse(resp.body)
   end
 end
--- test/jobs/block_chain_job_test.rb
+++ test/jobs/block_chain_job_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class BlockChainJobTest < ActiveJob::TestCase
   # 과제1 "블록 해쉬 값"을 Argument로 전달 받아서 아래의 정보를 출력
-  # 트랜잭션(tx) 수
+  # 트랜잭션(tx)의 수
   # 평균 트랜잭션의 값(value)
   # 평균 트랜잭션의 수수료(fee)
   # 평균 트랜잭션의 크기(size)
@@ -15,5 +15,12 @@ class BlockChainJobTest < ActiveJob::TestCase
     result_tx_count = block.total_tx_count(block_hash)
     puts "트랜잭션(tx)수: #{result_tx_count}"
     assert_equal correct_tx_count, result_tx_count
+
+    # 평균 트랜잭션의 값(value)이 맞는지 확인
+    ## 블록구조 정보에 나온 총 트랜잭션값과, json으로 받은 트랜잭션값의 단위가 달라 100,000,000을 곱해줍니다.
+    correct_avg_value = ((3746.58530944 * 100000000) / block.total_tx_count(block_hash).to_f).round(5)
+    result_avg_value = block.avg_tx_value(block_hash).round(5)
+    puts "평균 트랜잭션의 값: #{result_avg_value}"
+    assert_equal correct_avg_value, result_avg_value
   end
 end
