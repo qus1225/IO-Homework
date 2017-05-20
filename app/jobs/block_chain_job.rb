1967e841ffc34b311d6d08e4fdbf4e9969e4a5c6
qus <qus1225@gmail.com>
2017-05-20 22:26:00 +0900

    테스트케이스 더미파일이 생성됩니다

---
+++ app/jobs/block_chain_job.rb
@@ -0,0 +1,7 @@
+class BlockChainJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/block_chain_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BlockChainJobTest < ActiveJob::TestCase
+  test "테스트 케이스 시작" do
+    assert true
+  end
+end
